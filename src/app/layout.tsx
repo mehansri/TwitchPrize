5f428708de751ae218bfcfcb37552816f7011f9a
Mehan Sri <mehan.sritharan@gmail.com>
2025-08-24 15:53:34 -0400

    mobile fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "User Authentication App",
   description: "Sign up and login with NextAuth",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default function RootLayout({
